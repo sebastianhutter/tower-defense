d1d3f05024c0ad7a0da91262598e13ed295d80ac
Sebastian Hutter <mail@sebastian-hutter.ch>
2023-06-11 17:43:13 +0200

    Add floors for all biomes

---
+++ scenes/floor/grass/grass_floor.gd
@@ -0,0 +1,34 @@
+extends Floor
+
+# ========
+# singleton references
+# ========
+
+# ========
+# export vars
+# ========
+
+# ========
+# class signals
+# ========
+
+# ========
+# class onready vars
+# ========
+
+# ========
+# class vars
+# ========
+
+# ========
+# godot functions
+# ========
+
+# ========
+# signal handler
+# ========
+
+# ========
+# class functions
+# ========
+
