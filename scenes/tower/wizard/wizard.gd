a66711b30b97370418578b2542f29cad7a629af0
Sebastian Hutter <mail@sebastian-hutter.ch>
2023-06-15 07:02:41 +0200

    Fix up template

--- scenes/tower/wizard/wizard.gd
+++ scenes/tower/wizard/wizard.gd
@@ -1,3 +1,34 @@
 extends Tower
 class_name WizardTower
 
+# ========
+# singleton references
+# ========
+
+# ========
+# export vars
+# ========
+
+# ========
+# class signals
+# ========
+
+# ========
+# class onready vars
+# ========
+
+# ========
+# class vars
+# ========
+
+# ========
+# godot functions
+# ========
+
+# ========
+# signal handler
+# ========
+
+# ========
+# class functions
+# ========
